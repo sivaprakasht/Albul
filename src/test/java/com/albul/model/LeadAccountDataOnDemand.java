02179c8a9a2ce85a9af8f19c7164bf0abcab8305
sthiyag <sivaprakash_thiyagarajan@trimble.com>
2016-02-27 15:12:40 +0530

    ghj
    ghjghjghj

---
+++ src/test/java/com/albul/model/LeadAccountDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.albul.model;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = LeadAccount.class)
+public class LeadAccountDataOnDemand {
+}
---
+++ src/test/java/com/albul/model/LeadAccountIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.albul.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = LeadAccount.class)
+public class LeadAccountIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
